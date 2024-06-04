2d9f90d85eadb7c4c8f4ffcec461709776294e4e
Fatih Gumus <fatihgumus72@gmail.com>
2024-06-05 00:06:53 +0300

    swift binding example

--- SwiftBinding/ContentView.swift
+++ SwiftBinding/ContentView.swift
@@ -8,16 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var backgroundColor : Color = Color.red
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            backgroundColor
+                .ignoresSafeArea(.all)
+            VStack {
+                ButtonView(renkKodu: $backgroundColor)
+            }
+            .padding()
         }
-        .padding()
+
+    }
+    
+//    var ButtonView : some View{
+//        Button(action: {
+//            
+//        }, label: {
+//            Text("Renk Değiştir")
+//                .frame(width: 200,height: 75)
+//                .foregroundColor(.white)
+//                .background(.black)
+//                .clipShape(RoundedRectangle(cornerRadius: 10))
+//            
+//        })
+//    }
+}
+
+struct ButtonView: View{
+    @Binding var renkKodu : Color
+    var body: some View{
+        VStack{
+            Button(action: {
+                if (renkKodu == Color.red) {
+                    renkKodu = Color.gray
+                }else if (renkKodu == Color.gray){
+                    renkKodu = Color.white
+                }else{
+                    renkKodu = Color.red
+                }
+             
+            }, label: {
+                Text("Renk Değiştir")
+                    .frame(width: 200,height: 75)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            })
+        }
     }
 }
+
 
 #Preview {
     ContentView()
