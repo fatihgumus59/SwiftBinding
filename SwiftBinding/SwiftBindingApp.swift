ca6964ff5ac2ed56f517c79efc087ea43d103ef1
Fatih Gumus <fatihgumus72@gmail.com>
2024-06-04 23:25:23 +0300

    Initial Commit

---
+++ SwiftBinding/SwiftBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBindingApp.swift
+//  SwiftBinding
+//
+//  Created by Fatih Gümüş on 4.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
